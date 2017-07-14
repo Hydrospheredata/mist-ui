970a7ab12d04449a4d768c8e01fc061e15a873a7
Maxim Kirilchuk <bedros@mail.ru>
2017-07-14 14:18:25 +0300

    pre release preparation fixes (#38)

---
+++ src/app/pipes/job-id-cut.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { JobIdCutPipe } from './job-id-cut.pipe';
+
+describe('JobIdCutPipe', () => {
+  it('create an instance', () => {
+    const pipe = new JobIdCutPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
