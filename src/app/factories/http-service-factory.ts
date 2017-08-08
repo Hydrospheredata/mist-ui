79d05984b7fb067d9e8dfa92bf3d055787cef89a
Zajs <aga.zajs@gmail.com>
2017-08-08 16:14:23 +0300

    Changed urls in css,services,ws-services to relative paths.

--- src/app/factories/http-service-factory.ts
+++ src/app/factories/http-service-factory.ts
@@ -2,9 +2,10 @@ import { XHRBackend } from '@angular/http';
 import { MistRequestOptions } from '@services/mist-request-options';
 import { HttpService } from '@services/http.service';
 import { LoaderService } from '@services/loader.service';
+import { Location} from '@angular/common';
 
-function httpServiceFactory(backend: XHRBackend, options: MistRequestOptions, loaderService: LoaderService ) {
-  return new HttpService(backend, options, loaderService);
+function httpServiceFactory(backend: XHRBackend, options: MistRequestOptions, location:Location, loaderService: LoaderService ) {
+  return new HttpService(backend, options, location, loaderService);
 }
 
 export { httpServiceFactory };
