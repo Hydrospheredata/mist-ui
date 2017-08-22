cd92645d477b3615244b88ef77a3e2f627021694
Borobov Pavel <blvp.me@gmail.com>
2017-08-22 11:53:06 +0300

    fix: missing comma

--- src/app/components/dialogs/dialog-add-context/dialog-add-context.component.ts
+++ src/app/components/dialogs/dialog-add-context/dialog-add-context.component.ts
@@ -18,7 +18,7 @@ export class DialogAddContextComponent implements OnInit, OnDestroy {
     sparkConfValue: '',
     sparkConfKey: '',
     sparkConfs: {},
-    downtime: ''
+    downtime: '',
     maxJobs: '',
     precreated: '',
     runOptions: '',
