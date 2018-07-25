3241fd4cdf1a24d17811be05642b81466a97cb8b
Maxim Tereshin <tereshin93@gmail.com>
2018-07-25 14:31:14 +0300

    Removed unused worker component code

--- src/app/modules/workers/components/workers-list.component/workers-list.component.ts
+++ src/app/modules/workers/components/workers-list.component/workers-list.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { Worker } from '@app/modules/shared/models';
-import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { MistState } from '@app/modules/core/reducers';
 import { Observable } from 'rxjs/Observable';
-// import { WorkersStore } from '@app/modules/core/stores/workers.store';
 import * as fromWorkers from '@app/modules/workers/reducers';
 import * as fromWorkersActions from '@app/modules/workers/actions';
 
@@ -13,42 +11,17 @@ import * as fromWorkersActions from '@app/modules/workers/actions';
     templateUrl: './workers-list.component.html',
     styleUrls: ['./workers-list.component.scss']
 })
-export class WorkersListComponent implements OnInit, OnDestroy {
-    public workers: Worker[];
-    public workerSubscriber;
+export class WorkersListComponent {
     public searchQ: String;
     public workers$: Observable<Worker[]>;
 
     constructor(
-        // private workersStore: WorkersStore,
-        // private router: Router
         private store: Store<MistState>
     ) {
         this.workers$ = this.store.select(fromWorkers.getAllWorkers);
     }
 
-    ngOnInit() {
-        // this.workersStore.getAll();
-        // this.workerSubscriber = this.workersStore.workers
-        //   .subscribe((workers) => {
-        //     if (!workers.length) {
-        //       this.router.navigate(['/clusters/workers/overview']);
-        //     } else {
-        //       this.router.navigate([`/clusters/workers/${workers[0].name}`]);
-        //     }
-        //     this.workers = workers;
-        //   });
-    }
-
     public deleteWorker(worker: Worker) {
         this.store.dispatch(new fromWorkersActions.Delete(worker.name));
-        // this.workersStore.delete(worker);
     }
-
-    ngOnDestroy() {
-        // if (this.workerSubscriber) {
-        //   this.workerSubscriber.unsubscribe();
-        // }
-    }
-
 }
