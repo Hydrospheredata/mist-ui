034ae8451900221648d27eacebb1dd803cf599b1
Maxim Tereshin <tereshin93@gmail.com>
2018-07-25 14:28:31 +0300

    Removed unused and old files from app components

--- src/app/components/functions/_index.ts
+++
@@ -1,3 +0,0 @@
-export * from '@app/components/functions/functions-item-detail/functions-item-detail.component';
-export * from '@app/components/functions/functions-wrapper/functions-wrapper.component';
-export * from '@app/components/functions/functions-list/functions-list.component';
